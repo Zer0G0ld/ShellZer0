52bc9ced717ff8500a7360b3b0a58745c964b60c
Zer0G0ld <matheus321trabalho@gmail.com>
2023-08-14 15:26:21 -0300

    novos

---
+++ .history/start_20230814141207.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+echo "Deseja iniciar o terminal Shell Zero? [Y/n]"
+echo "$ "
+read -r terminal
+
+if [ "$terminal" = "y" ];then
+    echo "Bem-vindo!"
+    echo "...."
+    python3 main.py
+elif [ "$terminal" = "n" ];then
+    echo "Okay ..."
+    echo ""
+    echo ":'["
+    exit
+else
+    echo "Não consegui entender."
+    echo "Tente novamente."
+    while true; do
+        echo "Deseja iniciar o terminal Shell Zero? [Y/n]"
+        echo "$ "
+        read -r terminal
+        if [  ]
+    done
+fi
---
+++ .history/start_20230814141659.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+echo "Deseja iniciar o terminal Shell Zero? [Y/n]"
+echo "$ "
+read -r terminal
+
+test "$terminal" = "" && 
+
+if [ "$terminal" = "y" ];then
+    echo "Bem-vindo!"
+    echo "...."
+    python3 main.py
+elif [ "$terminal" = "n" ];then
+    echo "Okay ..."
+    echo ""
+    echo ":'["
+    exit
+else
+    echo "Não consegui entender."
+fi
